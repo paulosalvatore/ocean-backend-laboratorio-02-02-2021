70bec5960b1297799581b897644aa7e128871f5f
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-02 19:34:29 -0300

    Arrow Functions e console.info quando servidor inicia

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.get('/mensagens', function (req, res) {
-  res.send('Exibir todas as mensagens.')
-})
+app.get('/mensagens', (req, res) => {
+  res.send('Exibir todas as mensagens.');
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.info('Servidor rodando em http://localhost:3000');
+});
